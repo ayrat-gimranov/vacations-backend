2994f668c15e719ddc55121887eb40f46b051ce7
Ayrat <gimranov45@gmail.com>
2022-09-06 06:31:40 -0700

    replaced random with search

--- src/middleware/fetchPhoto.js
+++ src/middleware/fetchPhoto.js
@@ -1,18 +1,23 @@
 'use strict';
 
+const API_KEY = process.env.ACCESS_KEY;
 const fetch = require('node-fetch');
 
 async function fetchPhoto(req, res, next) {
   const defaultPhotoUrl = "https://c.pxhere.com/photos/a7/e3/red_rock_jeep_canoe_transport_vehicle-1392511.jpg!d";
-  const imageWidth = 720;
-  const imageHeight = 720;
+  const url = `https://api.unsplash.com/search/photos?query=${req.body.destination},${req.body.location}&orientation=landscape&content_filter=high`;
   let photo;
   try {
-    let response = await fetch(`https://source.unsplash.com/random/${imageWidth}×${imageHeight}/?${req.body.destination},${req.body.location}`);
-    if (response.url.startsWith("https://images.unsplash.com/source-404")) {
-      throw new Error("Couldn't find an image")
-    }
-    photo = response.url;
+    let response = await fetch(url, {
+      headers: {
+        'Authorization': `Client-ID ${API_KEY}` 
+      }
+    });
+    const results = (await response.json()).results;
+    const max = results.length;
+    const random = Math.floor((Math.random() * max));
+    const randomImg = results[random].urls.small; 
+    photo = randomImg;
   } catch (error) {
     console.log(error);
     photo = defaultPhotoUrl;
